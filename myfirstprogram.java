6d8e9ae43e0206f45a63e534f9d000b4a99148e0
9xworks <9xworks@Rishabhs-MacBook-Pro.local>
2020-04-19 11:24:13 +0530

    hese changes are fine

--- myfirstprogram.java
+++ myfirstprogram.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This will be printed");
-    }
+   i     System.out.println("This will be printed");
+    
+	hello ewoejw ;;
+}
 }
